df72d4d2bb49fd2bdb368a7af0d6c076c6b97398
Jakob <jakobfrontend@gmail.com>
2020-08-05 14:38:47 +0800

    Updates

---
+++ precache-manifest.dc7c5a12f591e50c7d4528559c0c8491.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf1ff899e238f29c289b1f2873f11dc0",
+    "url": "/bootstrap-5-ex/index.html"
+  },
+  {
+    "revision": "05716432073a095c2bab",
+    "url": "/bootstrap-5-ex/static/css/2.b07ff35c.chunk.css"
+  },
+  {
+    "revision": "ba9780b75a066f70e80e",
+    "url": "/bootstrap-5-ex/static/css/main.2412f46d.chunk.css"
+  },
+  {
+    "revision": "05716432073a095c2bab",
+    "url": "/bootstrap-5-ex/static/js/2.87148b13.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/bootstrap-5-ex/static/js/2.87148b13.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9780b75a066f70e80e",
+    "url": "/bootstrap-5-ex/static/js/main.86543457.chunk.js"
+  },
+  {
+    "revision": "999e8cb7c7054bc47b14",
+    "url": "/bootstrap-5-ex/static/js/runtime-main.01b9a65e.js"
+  },
+  {
+    "revision": "08e53a516d2ba719d98da46c49b3c369",
+    "url": "/bootstrap-5-ex/static/media/NunitoSans-Bold.08e53a51.ttf"
+  },
+  {
+    "revision": "cf8d9c6c81866d3bdfc1f08d6ea80d8d",
+    "url": "/bootstrap-5-ex/static/media/NunitoSans-ExtraLightItalic.cf8d9c6c.ttf"
+  },
+  {
+    "revision": "2d517b40dabe232416b73e3a721dc950",
+    "url": "/bootstrap-5-ex/static/media/NunitoSans-Italic.2d517b40.ttf"
+  },
+  {
+    "revision": "74d36921be67fb8482bfd7324bd86790",
+    "url": "/bootstrap-5-ex/static/media/NunitoSans-Light.74d36921.ttf"
+  }
+]);
